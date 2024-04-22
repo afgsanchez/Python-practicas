f7cc91bec53527d68c254e8b18521c7c2d5980c9
Antonio Gutierrez Sanchez <afgsanchez@gmail.com>
2024-04-22 15:06:53 +0200

    v 1.0.1

---
+++ bucle_while.py
@@ -0,0 +1,7 @@
+a = 0
+while a < 10:
+    print(a, end= " ") # El end= le indica que me imprima los valores de
+                       # a en la misma linea, si no lo ponemos nos los
+                       # imprimira uno debajo del otro.
+    a = a + 1
+print (a)
